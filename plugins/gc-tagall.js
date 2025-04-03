1bd2b4e668add7f720da0e56aa9772fcd59b4be4
Don Michifuz <Arm0899@outlook.es>
2025-04-02 20:32:57 -0600

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -1,21 +1,18 @@
 let handler = async(m, { isOwner, isAdmin, conn, text, participants, args, command }) => {
-  if (!(isAdmin || isOwner)) {
-  global.dfail('admin', m, conn)
-  throw false
-  }
-  let pesan = args.join` `
-  let oi = `_*Don Michifuz*_
-  
-   ${pesan}`
-  let teks = ` Mensaje: ${oi}EY DESPIERTEN\n`
-  for (let mem of participants) {
-  teks += `⚜️ @${mem.id.split('@')[0]}\n`}
-  teks += `ㅤㅤㅤㅤ@ Michifuz`
-  conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
-  }
-  handler.help = ['tagall <mesaje>','invocar <mesaje>']
-  handler.tags = ['group']
-  handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
-  handler.admin = true
-  handler.group = true
-  export default handler
+    if (!(isAdmin || isOwner)) {
+    global.dfail('admin', m, conn)
+    throw false
+    }
+    let pesan = args.join` `
+    let oi = `*MENSAJE:* ${pesan}`
+    let teks = `*DON MICHIFUZ LOS INVOCA*\n${oi}\n\n*MENCIONES*\n`
+    for (let mem of participants) {
+    teks += `👻 @${mem.id.split('@')[0]}\n`}
+    teks += `${wm}`
+    conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
+    }
+    handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+    handler.admin = true
+    handler.group = true
+    handler.botAdmin = true
+    export default handler
